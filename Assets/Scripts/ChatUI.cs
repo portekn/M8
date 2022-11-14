0321643d64c3d2f518298d905a0c991b4e16e475
Kelli Porter <77407178+portekn@users.noreply.github.com>
2022-11-13 21:09:46 -0500

    M8 - Chat Function Part 1
    Uploading working code.

---
+++ Assets/Scripts/ChatUI.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Netcode;
+using UnityEngine.UI;
+
+public class ChatUI : NetworkBehaviour
+{
+
+    public TMPro.TMP_Text txtChatLog;
+    public Button btnSend;
+    public TMPro.TMP_InputField inputMessage;
+
+    public void Start(){
+        btnSend.onClick.AddListener(ClientOnSendClicked);
+        inputMessage.onSubmit.AddListener(ClientOnInputSubmit);
+    }
+
+    public override void OnNetworkSpawn(){
+        txtChatLog.text = "-- StartChatLog --";
+        if(IsHost){
+            NetworkManager.Singleton.OnClientConnectedCallback += HostOnClientConnected;
+            NetworkManager.Singleton.OnClientDisconnectCallback += HostOnClientDisconnected;
+            DisplayMessageLocally("You are the host");
+        }
+        else{
+            DisplayMessageLocally($"You are Player #{NetworkManager.Singleton.LocalClientId}");
+        }
+    }
+
+    private void SendUIMessage(){
+        string msg = inputMessage.text;
+        inputMessage.text = "";
+        SendChatMessageServerRpc(msg);
+    }
+
+    //Events
+    private void HostOnClientConnected(ulong clientId){
+        SendChatMessageClientRpc($"Client {clientId} connected");
+    }
+
+    private void HostOnClientDisconnected(ulong clientId){
+        SendChatMessageClientRpc($"Client {clientId} disconnected");
+    }
+
+    public void ClientOnSendClicked(){
+        SendUIMessage();
+    }
+
+    public void ClientOnInputSubmit(string text){
+        SendUIMessage();
+    }
+
+    //RPC
+    [ClientRpc]
+    public void SendChatMessageClientRpc(string message, ClientRpcParams clientRpcParams = default){
+        DisplayMessageLocally(message);
+    }
+
+    [ServerRpc (RequireOwnership = false)]
+    public void SendChatMessageServerRpc(string message, ServerRpcParams serverRpcParams = default){
+        Debug.Log($"Host got message:  {message}");
+        string newMessage = $"[player #{serverRpcParams.Receive.SenderClientId}]: {message}";
+        SendChatMessageClientRpc(newMessage);
+    }
+
+    public void DisplayMessageLocally(string message){
+        Debug.Log(message);
+        txtChatLog.text += $"\n{message}";
+    }
+}
